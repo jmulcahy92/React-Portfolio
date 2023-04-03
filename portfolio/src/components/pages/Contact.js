a4684a9964bee4e0544d7c42c5ba626bd3604811
jmulcahy92 <jacobkmulcahy@gmail.com>
2023-04-03 00:12:55 -0500

    contact form with error messages

--- portfolio/src/components/pages/Contact.js
+++ portfolio/src/components/pages/Contact.js
@@ -1,21 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { validateEmail } from '../../utils/helpers';
 
 export default function Contact() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleInputChange = (e) => {
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+    
+        if (inputType === 'name') {
+            setName(inputValue);
+        } else if (inputType === 'email') {
+            setEmail(inputValue);
+        } else {
+            setMessage(inputValue);
+        }
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+    
+        if (!validateEmail(email)) {
+            setErrorMessage('Email is invalid');  
+        }
+
+        setName('');
+        setEmail('');
+        setMessage('');
+    };
+
+    const handleInputExit = (e) => {
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+        if (inputValue == "") {
+            setErrorMessage(`Please input a ${inputType}`)
+        }
+    }
+
     return (
         <main>
-            <p>
-                Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-                molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-                magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-                efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-                mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-                posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-                faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-                ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-                dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-                conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-                rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-            </p>
+            <form className="form d-flex flex-column m-3">
+                <div className="mb-3">
+                    <label for="name" className="form-label">Name</label>
+                    <input
+                        value={name}
+                        name="name"
+                        onChange={handleInputChange}
+                        onMouseOut={handleInputExit}
+                        type="text"
+                        placeholder="Ada Lovelace"
+                        className="form-control"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label for="email" className="form-label">Email</label>
+                    <input
+                        value={email}
+                        name="email"
+                        onChange={handleInputChange}
+                        onMouseOut={handleInputExit}
+                        type="email"
+                        placeholder="name@example.com"
+                        className="form-control"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label for="message" className="form-label">Message</label>
+                    <textarea 
+                        value={message}
+                        name="message"
+                        onChange={handleInputChange}
+                        onMouseOut={handleInputExit}
+                        type="text"
+                        className="form-control"
+                        rows="3"
+                    />
+                </div>
+                <button type="submit" className="btn btn-primary" onClick={handleFormSubmit}>Submit</button>
+                {errorMessage && (
+                <div>
+                    <p className="error-text">{errorMessage}</p>
+                </div>
+            )}
+            </form>
         </main>
     );
 }
